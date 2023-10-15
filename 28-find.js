b1a7f0ebfa5c4ed89a0f4099de008975cb936820
Jordan Higuera <jordanwhiguera@gmail.com>
2023-10-15 14:59:31 -0400

    28-find

---
+++ 28-find.js
@@ -0,0 +1,5 @@
+//SOLVED: Time O(n * m) Space O(1)
+function strStr(haystack, needle) {
+  //This function will return the index of the first occurrence of needle in haystack or -1 if needle is not part of haystack.
+  return haystack.indexOf(needle);
+}
