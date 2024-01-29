f6e122376759da6bee9744706a245dd14efe0492
Jordan Higuera <jordanwhiguera@gmail.com>
2024-01-28 22:03:11 -0500

    205-isomorphic

---
+++ 205-isomorphic.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+//Time Complexity is O(n) and Space complexity is O(1)
+function isIsomorphic(s, t) {
+  let mapST = {};
+  let mapTS = {};
+  for (let i = 0; i < s.length; i++) {
+    if (
+      (mapST[s[i]] && mapST[s[i]] !== t[i]) ||
+      (mapTS[t[i]] && mapTS[t[i]] !== s[i])
+    ) {
+      return false;
+    }
+    mapST[s[i]] = t[i];
+    mapTS[t[i]] = s[i];
+  }
+  return true;
+}
